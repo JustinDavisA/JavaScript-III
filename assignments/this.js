9fef986ca5b3b2d695af92d89a25a66be56a2659
justindavisa <justindavisa@gmail.com>
2019-01-23 16:02:28 -0500

    Filled this.js questionaire

--- assignments/this.js
+++ assignments/this.js
@@ -1,14 +1,20 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Globally 'this' === the console or window
+* 2. When using functions objects before the '.function' are 'this' 
+* 3. Constructors return new instances of an object
+* 4. 'this' is explicity defined for call and apply methods
 *
 * write out a code example of each explanation above
 */
+console.log(this);
 
+console.log(`You are such a ${this.playfulInsult}`);
+
+const justin = new Employee('Justin');
+
+justin.eat.call(actions); actions.eat.apply(justin);
 // Principle 1
 
 // code example for Window Binding
